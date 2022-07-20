5150e458cb99201e2daca5d51d09279f2f687ec2
DANIEL TAPP <danieltapp@DANIELs-MacBook-Pro-2.local>
2022-07-19 20:44:52 -0400

    setup prisma & graphQL

---
+++ directory/schema.ts
@@ -0,0 +1,29 @@
+import { createModule, gql } from "graphql-modules"
+import { prismaClient } from "../prisma"
+
+export const directoryModule = createModule({
+  id: "directory-module",
+  dirname: __dirname,
+  typeDefs: gql`
+    type Directory implements FileNode {
+      id: ID!
+      name: String!
+      parentId: ID
+      createdAt: String!
+      updatedAt: String!
+      files: [File]!
+      directories: [Directory]!
+    }
+
+    extend type Query {
+      getAllDirectories: [Directory]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      getAllDirectories: async () => {
+        return await prismaClient().directory.findMany()
+      },
+    },
+  },
+})
---
+++ file/schema.ts
@@ -0,0 +1,28 @@
+import { createModule, gql } from "graphql-modules"
+import { prismaClient } from "../prisma"
+
+export const fileModule = createModule({
+  id: "file-module",
+  dirname: __dirname,
+  typeDefs: gql`
+    type File implements FileNode {
+      id: ID!
+      name: String!
+      directoryId: ID
+      createdAt: String!
+      updatedAt: String!
+      versions: [FileVersion]!
+    }
+
+    extend type Query {
+      getAllFiles: [File]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      getAllFiles: async () => {
+        return await prismaClient().file.findMany()
+      },
+    },
+  },
+})
